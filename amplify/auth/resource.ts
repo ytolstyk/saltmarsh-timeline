9f5dda6b2b78987b344a6d125c40cbeeeeb534e5
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2025-06-27 17:27:20 -0700

    ignoring google for now

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth, secret } from "@aws-amplify/backend";
+import { defineAuth } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,24 +7,24 @@ import { defineAuth, secret } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    externalProviders: {
-      google: {
-        clientId: secret("GOOGLE_CLIENT_ID"),
-        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
-        scopes: ["email", "profile"],
-      },
-      callbackUrls: [
-        "http://localhost:5173/",
-        "https://saltmarsh-timeline.com/",
-        "https://www.saltmarsh-timeline.com/",
-        "https://dev.d1o0zj6tq2dxh8.amplifyapp.com/",
-      ],
-      logoutUrls: [
-        "http://localhost:5173/",
-        "https://saltmarsh-timeline.com",
-        "https://www.saltmarsh-timeline.com",
-        "https://dev.d1o0zj6tq2dxh8.amplifyapp.com",
-      ],
-    },
+    // externalProviders: {
+    //   google: {
+    //     clientId: secret("GOOGLE_CLIENT_ID"),
+    //     clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+    //     scopes: ["email", "profile"],
+    //   },
+    //   callbackUrls: [
+    //     "http://localhost:5173/",
+    //     "https://saltmarsh-timeline.com/",
+    //     "https://www.saltmarsh-timeline.com/",
+    //     "https://dev.d1o0zj6tq2dxh8.amplifyapp.com/",
+    //   ],
+    //   logoutUrls: [
+    //     "http://localhost:5173",
+    //     "https://saltmarsh-timeline.com",
+    //     "https://www.saltmarsh-timeline.com",
+    //     "https://dev.d1o0zj6tq2dxh8.amplifyapp.com",
+    //   ],
+    // },
   },
 });
